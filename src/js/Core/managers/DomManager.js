d33376762a8d8bc80a3630c3e99e672c99cd93a9
Théo <tochampion38@gmail.com>
2020-10-22 20:45:52 +0200

    Add overlay

--- src/js/Core/managers/DomManager.js
+++ src/js/Core/managers/DomManager.js
@@ -7,16 +7,32 @@ class DomManager {
 
     initElements() {
         this.TEMPLATE_DIRECTORY = '../../templates';
+        this.overlayActivity = document.getElementById('activity-overlay');
         this.activityContainer = document.getElementById('activity-container');
     }
 
     async inject(templateName) {
         try {
+            await this.displayOverlay();
             const {data: templateContent} = await axios.get(`${this.TEMPLATE_DIRECTORY}/${templateName}`);
             this.activityContainer.innerHTML = templateContent;
+            this.hideOverlay();
         } catch (e) {
             console.log(`Template ${templateName} not found`);
         }
+    }
+
+    displayOverlay(time = 3000) {
+        this.overlayActivity.style.width = '100%';
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, time)
+        });
+    }
+
+    hideOverlay() {
+        this.overlayActivity.style.width = '0%';
     }
 }
 
