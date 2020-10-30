d80a0b7b6e541e9086e2a12233a28f2fc18cd709
Théo <tochampion38@gmail.com>
2020-10-30 19:50:03 +0100

    Finish integration of first activity

--- src/js/Core/routes/Router.js
+++ src/js/Core/routes/Router.js
@@ -20,15 +20,29 @@ export default class Router {
         return this;
     }
 
-    init() {
+    init(testTargetActivity = null) {
         for (const route of this.routes) {
             this.webSocketClient.on(route.event, async () => {
                 console.log(route.event);
-                const Activity = new route.activity(this.webSocketClient);
-                const templateName = Activity.getTemplate();
-                await DomManager.inject(templateName);
-                Activity.launch();
+                await this._displayActivity(route.activity);
             });
         }
+
+        if (testTargetActivity) {
+            this._displayActivity(testTargetActivity)
+                .then(() => {
+                    console.log(`Test activity displayed successfully`);
+                })
+                .catch(e => {
+                    console.log('An error has occured during the activity test launch !', e);
+                })
+        }
+    }
+
+    async _displayActivity(activity) {
+        const Activity = new activity(this.webSocketClient);
+        const templateName = Activity.getTemplate();
+        await DomManager.inject(templateName);
+        Activity.launch();
     }
 }
